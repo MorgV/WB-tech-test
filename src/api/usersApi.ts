60a825daf3aa8498979296f964ad8d5453fe4877
MorgV <erikhan03@mail.ru>
2025-10-21 06:43:56 +0300

    feat(api): добавлены методы create, update и delete для пользователей

--- src/api/usersApi.ts
+++ src/api/usersApi.ts
@@ -17,11 +17,24 @@ const api = axios.create({
 
 export const usersApi = {
   getAll: async (): Promise<User[]> => {
+    console.log("getAll");
     const res = await api.get<User[]>("/");
     return res.data;
   },
   getById: async (id: string): Promise<User> => {
+    console.log("getBiId: ", id);
     const res = await api.get<User>(`/${id}`);
     return res.data;
+  },
+  create: async (user: Partial<User>): Promise<User> => {
+    const res = await api.post<User>("/", user);
+    return res.data;
+  },
+  update: async (id: string, user: Partial<User>): Promise<User> => {
+    const res = await api.put<User>(`/${id}`, user);
+    return res.data;
+  },
+  remove: async (id: string): Promise<void> => {
+    await api.delete(`/${id}`);
   },
 };
