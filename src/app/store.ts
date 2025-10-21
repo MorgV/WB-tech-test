88e03c13d6aaf7c4143ebf43aaf24f0d58beb773
MorgV <erikhan03@mail.ru>
2025-10-21 06:45:18 +0300

    feat(users): настройка Redux store с redux-persist и создание слайса пользователей

---
+++ src/app/store.ts
@@ -0,0 +1,43 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import usersReducer from "../features/users/usersSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  users: usersReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["users"],
+  devTools: process.env.NODE_ENV !== "production",
+  blacklist: ["users.error", "users.loading"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: true,
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store, persistor } from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
+import CssBaseline from "@mui/material/CssBaseline";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <CssBaseline />
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
