76cd33fff15dcf0e9925b6fbf6a5a2b471d25bf7
MorgV <erikhan03@mail.ru>
2025-10-22 15:49:11 +0300

    123

--- src/shared/UI/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./GlowButton";
-export * from "./PageContainer";
-export * from "./StarAvatar";
-export * from "./StarBox";
-export * from "./StarTableCell";
-export * from "./StarBackground";
